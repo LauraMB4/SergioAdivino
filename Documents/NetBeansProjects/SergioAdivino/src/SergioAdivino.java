980e8d13a446df65d4a3d5fb72a979e7ac193bad
pelop <pelop@RayDark>
2019-11-11 13:36:07 +0100

    Creamos la rama cambio y modificamos los valores de 10 a 50 para que nos genere un número aleatorio entre estos dos valores

---
+++ Documents/NetBeansProjects/SergioAdivino/src/SergioAdivino.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class SergioAdivino {
+
+    public static void main(String[] args) {
+        int n = 0,cont = 0;
+        int x = (int)(Math.random()*(50-10+1)+10);
+        try ( //System.out.print(x);
+                Scanner sc = new Scanner(System.in)) {
+            do {
+                System.out.print("\nIntroduce el valor de X: ");
+                n = sc.nextInt();
+                if (n > x) {
+                    System.out.print("Te pasaste Amigo");
+                } else if (n < x){
+                    System.out.print("Casi cerca ...");
+                }
+                cont++;
+            } while (n != x);
+            System.out.print("Exelente!!! Haz acertado , No. de intentos: "+cont );
+        }
+    }
+}
